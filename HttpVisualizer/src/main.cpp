164049fb35992363c100596a2838dfc93f6400bf
pyvyx <pyvyx@web.de>
2022-11-03 21:05:58 +0100

    Added ability to receive get response as std::string

--- HttpVisualizer/src/main.cpp
+++ HttpVisualizer/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <cstdio>
 
 #define RAYGUI_IMPLEMENTATION
@@ -6,33 +7,44 @@
 #include "raygui.h"
 #include "curl/curl.h"
 
+/*
+    http://www.boredapi.com/api/activity
+    https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json
+*/
+
+
+size_t CurlWrite_CallbackFunc_StdString(void* contents, size_t size, size_t nmemb, std::string* s)
+{
+    const size_t newLength = size * nmemb;
+    s->append(static_cast<char*>(contents), newLength);
+    return newLength;
+}
+
 
 int main()
 {
     InitWindow(600, 500, "HttpVisualizer");
-    
-    CURL* curl;
-    CURLcode res;
 
+
+    std::string s;
     curl_global_init(CURL_GLOBAL_DEFAULT);
-
-    curl = curl_easy_init();
-    if (curl) {
+    CURL* curl = curl_easy_init();
+    if (curl != NULL)
+    {
         curl_easy_setopt(curl, CURLOPT_URL, "http://www.boredapi.com/api/activity");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, CurlWrite_CallbackFunc_StdString);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
         //curl_easy_setopt(curl, CURLOPT_URL, "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json");
 
 
-        /* Perform the request, res will get the return code */
-        res = curl_easy_perform(curl);
-        /* Check for errors */
+        CURLcode res = curl_easy_perform(curl);
         if (res != CURLE_OK)
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                curl_easy_strerror(res));
-
-        /* always cleanup */
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         curl_easy_cleanup(curl);
     }
     curl_global_cleanup();
+    std::cout << s << std::endl;
+
 
     while (!WindowShouldClose())
     {
@@ -43,3 +55,7 @@ int main()
     TerminateWindow();
     return 0;
 }
+
+
+//curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); //only for https
+//curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L); //only for https
