de07dc9b7ead1ec94dc003f96c232388be778708
pyvyx <pyvyx@web.de>
2023-05-08 00:07:54 +0200

    Cleanup all the nesseccary code for C

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 #![allow(non_snake_case)]
 
+fn Request(url: &str) -> Result<String, reqwest::Error>
+{
+    /*
+        http://www.boredapi.com/api/activity
+        https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json
+    */
+    return Ok(reqwest::blocking::get(url)?.text()?);
+}
+
 fn main()
 {
-    println!("Hello, world!");
+    match Request("..")
+    {
+        Ok(response) => println!("{}", response),
+        Err(e)  => return println!("Failed to make request: '{}'", e),
+    }
 }
